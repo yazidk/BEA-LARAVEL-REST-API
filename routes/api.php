4abcf4a07b84e6b2b36bee02d7836119501cd189
bernou <bernou_y@yahoo.fr>
2023-02-13 22:51:28 +0100

    solving fiche problem

--- routes/api.php
+++ routes/api.php
@@ -5,26 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\Fiche_tenue_compteController;
 
-<<<<<<< HEAD
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-//Public Routes
-Route::post('/register' , [AuthController::class , 'register']);
 
-
-//Protected Routes
-Route::group(['middleware' => ['auth:sanctum']] , function () {
-    Route::post('/ajouter-fiche' , [Fiche_tenue_compteController::class , 'store']);
-    Route::post('/logout' , [Fiche_tenue_compteController::class , 'logout']); 
-=======
 
 // Public routes 
 Route::post('/register' , [AuthController::class , 'register']);
@@ -36,6 +17,6 @@ Route::post('/ajouter-fiche' , [Fiche_tenue_compteController::class , 'store']);
 Route::group(['middleware' => ['auth:sanctum']] , function () {
    
     Route::post('/logout' , [AuthController::class , 'logout']);
->>>>>>> origin/yazid_dev
+
 });
 
